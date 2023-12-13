2425ec4736d256077ffcf2b7652c2223aac56cf2
arpitm565 <153750314+arpitm565@users.noreply.github.com>
2023-12-13 20:51:48 +0530

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,27 @@
+
+import random as r
+a = (input("Greetings user! Would you like to roll the dice ? , Yes or No " ).lower())
+def inputchecker(a) :
+    if a == "yes":
+        print("You have entered yes, lets continue!")
+        return True
+    elif a == "no":
+        print('Thank you, and have a ncie day!')
+        return False
+    else :
+        raise ValueError(" Invalid input. Kindly enter yes or no only. ")
+        return False
+if inputchecker(a)  :
+   v1 =   r.randint(1,6)
+   print("The value appeared is : ", v1 )
+   if v1 == 6 :
+       v2 =  r.randint(1,6)
+       print("The second value appeared is : ", v2 )
+       if v2 == 6 :
+         print("YAY! you got a second roll")
+         v3 =   r.randint(1,6)
+         print("The third value appeared is : ", v3 )
+         if v3 == 6 :
+             print("YAY! third roll! ")
+             print('Oh! only 3 6s are allowed') 
+     
